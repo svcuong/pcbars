a8763362901d1b461b294764feff8717a4ee238d
Cuong.S <31816408+svcuonghvktqs@users.noreply.github.com>
2018-03-06 06:48:27 -0800

    fixed documentation

--- R/pcbars-package.R
+++ R/pcbars-package.R
@@ -4,13 +4,11 @@ NULL
 
 #' Estimate percentage errors
 #'
-#'Contains tools to measure and visualise the uncertainty around the
-#`percentages used to represent relative frequencies. These are useful
-#`when performing descriptive analysis of categorical variables.
+#'Contains tools to measure and visualise the uncertainty around the percentages used to represent relative frequencies. These are useful when performing descriptive analysis of categorical variables.
 #'
 #'
 #' @details{\tabular{ll}{ Package: \tab pcbars\cr Type: \tab Package\cr License: \tab GPL3\cr}}
-#' @name M3FORA-package
+#' @name pcbars-package
 #' @aliases pcbars-package pcbars
 #' @docType package
 #' @author Andrey Davydenko, Maxim Shcherbakov and Sai Van Cuong from Volgograd State Technical University
